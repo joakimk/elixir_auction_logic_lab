9c33051eeafa86002c7007416c7babbdc7ec0ce2
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-12-29 20:34:09 +0100

    Make it possible to close an auction

--- lib/auction.ex
+++ lib/auction.ex
@@ -14,23 +14,44 @@ defmodule Auction do
 
   def place_bid(id, amount: amount, bidder_id: bidder_id) do
     identifier(id)
-    |> Agent.update(fn state ->
-      if state.leading_bid && amount < state.leading_bid.amount do
-        state
-      else
+      |> Agent.update(fn state ->
         bid = %{ id: state.next_bid_id, amount: amount, bidder_id: bidder_id }
-
-        state
-        |> Map.put(:leading_bid, bid)
-        |> Map.put(:bids, [bid | state.bids])
-        |> Map.put(:next_bid_id, state.next_bid_id + 1)
-      end
-    end)
+        do_place_bid(state, bid)
+      end)
   end
 
   def get_state(id) do
     identifier(id)
     |> Agent.get(fn state -> state end)
+  end
+
+  defp do_place_bid(state, bid) do
+    if allowed_to_place_bid?(state, bid) do
+      update_bids state, bid
+    else
+      state
+    end
+  end
+
+  defp allowed_to_place_bid?(state, bid) do
+    auction_ongoing?(state) &&
+      bid_above_leading_bid?(bid, state.leading_bid)
+  end
+
+  defp auction_ongoing?(state) do
+    :erlang.system_time < state.end_time
+  end
+
+  defp bid_above_leading_bid?(_, nil), do: true
+  defp bid_above_leading_bid?(bid, leading_bid) do
+    bid.amount > leading_bid.amount
+  end
+
+  defp update_bids(state, bid) do
+    state
+    |> Map.put(:leading_bid, bid)
+    |> Map.put(:bids, [bid | state.bids])
+    |> Map.put(:next_bid_id, state.next_bid_id + 1)
   end
 
   defp identifier(id) do
--- test/auction_test.exs
+++ test/auction_test.exs
@@ -3,7 +3,7 @@ defmodule AuctionTest do
   doctest Auction
 
   setup do
-    Auction.start 1, %{ bids: [], leading_bid: nil, next_bid_id: 1 }
+    Auction.start 1, %{ bids: [], leading_bid: nil, next_bid_id: 1, end_time: :erlang.system_time + 10000000}
     :ok
   end
 
@@ -35,5 +35,21 @@ defmodule AuctionTest do
 
     state = Auction.get_state(1)
     assert state.leading_bid.amount == 200
+  end
+
+  test "does not change state for equal bids" do
+    Auction.place_bid(1, amount: 200, bidder_id: 10)
+    Auction.place_bid(1, amount: 200, bidder_id: 15)
+
+    state = Auction.get_state(1)
+    assert state.leading_bid.amount == 200
+    assert Enum.count(state.bids) == 1
+  end
+
+  test "does not change state for late bids" do
+    Auction.start 2, %{ bids: [], leading_bid: nil, next_bid_id: 1, end_time: :erlang.system_time - 10000 }
+    Auction.place_bid(2, amount: 200, bidder_id: 10)
+    state = Auction.get_state(2)
+    assert state.leading_bid == nil
   end
 end
